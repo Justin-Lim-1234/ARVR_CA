e83d8974938765b47ba58bce155e0f8f8b295215
Ray Chng <10988b@gmail.com>
2020-05-14 21:24:11 +0800

    Added Waypoints and Gamemanger Scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    public GameObject RobotPrefab;
+    public GameObject SpawnPoint1;
+    public GameObject SpawnPoint2;
+    public GameObject SpawnPoint3;
+    private Waypoints robotvalues;
+    private int count= 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+     SpawnRobotLvl3();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void SpawnRobotLvl1()
+    {
+        DestroyExistingRobot();
+        count = 1;
+        for (int i = 0; i < count; i++)
+        {
+            GameObject robot = Instantiate(RobotPrefab, SpawnPoint1.transform);
+            robotvalues = robot.GetComponent<Waypoints>();
+            robotvalues.LevelofBot = 1;
+            robotvalues.speed = 2;
+        }
+
+
+    }
+
+    public void SpawnRobotLvl2()
+    {
+        DestroyExistingRobot();
+        count = 1;
+        for (int i = 0; i < count; i++)
+        {
+            GameObject robot = Instantiate(RobotPrefab, SpawnPoint2.transform);
+            robotvalues = robot.GetComponent<Waypoints>();
+            robotvalues.LevelofBot = 2;
+            robotvalues.speed = 4;
+        }
+
+
+    }
+
+    public void SpawnRobotLvl3()
+    {
+        DestroyExistingRobot();
+        count = 1;
+        for (int i = 0; i < count; i++)
+        {
+            GameObject robot = Instantiate(RobotPrefab, SpawnPoint3.transform);
+            robotvalues = robot.GetComponent<Waypoints>();
+            robotvalues.LevelofBot = 3;
+            robotvalues.speed = 7;
+            robotvalues.rangeWaypoints = 0.7f;
+        }
+
+
+    }
+
+
+    public void DestroyExistingRobot()
+    {
+        GameObject[] robots = GameObject.FindGameObjectsWithTag("Robot");
+
+        for (int i = 0; i < robots.Length; i++)
+        {
+
+            Destroy(robots[i]);
+        }
+
+    }
+}
+
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    private GameObject [] LevelWaypoints;
+    public int LevelofBot = 0;
+    public float  rangeWaypoints = 2;
+    private GameObject currentWaypointTarget;
+    private int index;
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = 0;
+        LevelWaypoints = GameObject.FindGameObjectsWithTag("WaypointsLevel" + LevelofBot);
+        currentWaypointTarget = LevelWaypoints[index];
+        print("No. of Waypoints is " + LevelWaypoints.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Vector3.Distance(transform.position, LevelWaypoints[index].transform.position) < rangeWaypoints)
+        {
+            index++;
+            if (index >= LevelWaypoints.Length)
+            {
+                index = 0;
+            }
+
+        }
+        else
+        {
+            // Move our position a step closer to the target.
+            float step = speed * Time.deltaTime; // calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, LevelWaypoints[index].transform.position, step);
+        }
+        
+      
+
+    }
+}

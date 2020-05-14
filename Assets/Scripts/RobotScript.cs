ff2d5d7c7e81e7967082f1860d1006eed6176b06
Justin Lim <justinlimweiyang@gmail.com>
2020-05-14 16:57:33 +0800

    gun can shoot

---
+++ Assets/Scripts/RobotScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotScript : MonoBehaviour
+{
+    private float health;
+    private bool dead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HealthManager();
+    }
+
+    public void TakeDamage(float dmg)
+    {
+        health -= dmg;
+    }
+
+    private void HealthManager()
+    {
+        if (health <= 0)
+        {
+            dead = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shoot : MonoBehaviour
+{
+    public Canvas ammoCanvas;
+    public Text ammoText;
+    private float ammoCount = 15f;
+    public Transform firingPt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShowAmmo();
+        ammoCanvas.transform.LookAt(Camera.main.transform);
+    }
+
+    public void GunShot()
+    {
+        RaycastHit hit;
+        if (ammoCount > 0)
+        {
+            if (Physics.Raycast(firingPt.transform.position, firingPt.transform.forward * 1000, out hit, 100))
+            {
+                RobotScript rbt = hit.transform.GetComponent<RobotScript>();
+
+                if (rbt != null)
+                {
+                    Debug.Log("Hit");
+                }
+            }
+            --ammoCount;
+        }
+    }
+
+    private void ShowAmmo()
+    {
+        ammoText.text = ammoCount.ToString();
+    }
+}
